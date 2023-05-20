c1e892e4efbd09d3386b003b63d84774ca96fe4d
webdesignborgman <info@webdesignborgman.nl>
2023-05-20 23:17:49 +0200

    Started on chaning register comp, no logic yet

--- app/components/Register.jsx
+++ app/components/Register.jsx
@@ -4,7 +4,7 @@ import { FcGoogle } from 'react-icons/fc';
 
 export default function Register({ onBackToLogin }) {
   return (
-    <div className="w-full mx-auto max-w-md m-4 p-4 rounded-md shadow sm:p-8 dark:dark:bg-gray-900 dark:dark:text-gray-100">
+    <div className="w-full mx-auto max-w-md m-4 p-4 rounded-md shadow sm:p-8 dark:dark:bg-gray-700 dark:dark:text-gray-100">
       <h2 className="mb-3 text-3xl font-semibold text-center">
         Register a new account
       </h2>
@@ -18,27 +18,26 @@ export default function Register({ onBackToLogin }) {
           Back to login
         </p>
       </h2>
-      <div className="my-6 space-y-4">
-        <button
-          aria-label="Login with Google"
-          type="button"
-          className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:dark:border-gray-400 focus:ring-violet-400"
-        >
-          <FcGoogle className="w-5 h-5 fill-current" />
-          <p>Register using Google</p>
-        </button>
-      </div>
-      <div className="flex items-center w-full my-4">
-        <hr className="w-full dark:dark:text-gray-400" />
-        <p className="px-3 dark:dark:text-gray-400">OR</p>
-        <hr className="w-full dark:dark:text-gray-400" />
-      </div>
+
       <form
         noValidate=""
         action=""
         className="space-y-8 ng-untouched ng-pristine ng-valid"
       >
         <div className="space-y-4">
+          <div className="space-y-2">
+            <label htmlFor="email" className="block text-sm">
+              Username
+            </label>
+            <input
+              type="text"
+              name="text"
+              id="text"
+              autoComplete="current-username"
+              placeholder="Leroy"
+              className="w-full px-3 py-2 border rounded-md dark:dark:border-gray-700 dark:dark:bg-gray-900 dark:dark:text-gray-100 focus:dark:dark:border-violet-400"
+            />
+          </div>
           <div className="space-y-2">
             <label htmlFor="email" className="block text-sm">
               Email address
@@ -75,6 +74,21 @@ export default function Register({ onBackToLogin }) {
           Register
         </button>
       </form>
+      <div className="flex items-center w-full my-4">
+        <hr className="w-full dark:dark:text-gray-400" />
+        <p className="px-3 dark:dark:text-gray-400">OR</p>
+        <hr className="w-full dark:dark:text-gray-400" />
+      </div>
+      <div className="my-6 space-y-4">
+        <button
+          aria-label="Login with Google"
+          type="button"
+          className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:dark:border-gray-400 focus:ring-violet-400"
+        >
+          <FcGoogle className="w-5 h-5 fill-current" />
+          <p>Login with Google</p>
+        </button>
+      </div>
     </div>
   );
 }
