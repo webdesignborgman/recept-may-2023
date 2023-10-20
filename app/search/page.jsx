a864b818b0c6615520e5de7d6d3526a655b9b41e
webdesignborgman <info@webdesignborgman.nl>
2023-10-20 19:21:31 +0200

    new search component

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -3,5 +3,7 @@
 import React from 'react';
 
 export default function searchPage() {
-  return <div className="max-w-md w-full mx-auto text-red-500">searchBar</div>;
+  return (
+    <div className="max-w-md w-full mx-auto text-red-500">Search Component</div>
+  );
 }
